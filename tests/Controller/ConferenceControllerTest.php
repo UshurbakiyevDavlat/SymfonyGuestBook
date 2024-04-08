3455f4c78a9fc391e4901966e37d29713c7d0ef3
UshurbakiyevDavlat <dushurbakiev@gmail.com>
2024-04-08 17:56:45 +0500

    added functional test for check index on home page

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+}
