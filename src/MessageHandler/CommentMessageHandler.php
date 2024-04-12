f33b9064582dc3e065b37dd7d1b41c72eb9b7d2f
UshurbakiyevDavlat <dushurbakiev@gmail.com>
2024-04-12 11:55:55 +0500

    added workflow state machine functionality, installed workflow visualisator, updated logic for spam checker workflow in comment message handler

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -8,7 +8,10 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 #[AsMessageHandler]
@@ -18,6 +21,9 @@ readonly class CommentMessageHandler
         private EntityManagerInterface $entityManager,
         private SpamChecker            $spamChecker,
         private CommentRepository      $commentRepository,
+        private MessageBusInterface    $bus,
+        private WorkflowInterface      $commentStateMachine,
+        private ?LoggerInterface       $logger = null,
     )
     {
     }
@@ -28,20 +34,38 @@ readonly class CommentMessageHandler
     public function __invoke(CommentMessage $message): void
     {
         $comment = $this->commentRepository->find($message->getId());
+
         if (!$comment) {
             return;
         }
 
-        $commentSpamScore = $this->spamChecker->getSpamScore($comment, $message->getContext());
+        if ($this->commentStateMachine->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = match ($score) {
+                2 => 'reject_spam',
+                1 => 'might_be_spam',
+                default => 'accept',
+            };
 
-        if (2 === $commentSpamScore) {
-            $comment->setState('spam');
-        } elseif (1 === $commentSpamScore) {
-            $comment->setState('maybeSpam');
+            $this->commentStateMachine->apply($comment, $transition);
+            $this->entityManager->flush();
+            $this->bus->dispatch($message);
+        } elseif (
+            $this->commentStateMachine->can($comment, 'publish')
+            ||
+            $this->commentStateMachine->can($comment, 'publish_ham')
+        ) {
+            $this->commentStateMachine->apply(
+                $comment,
+                $this->commentStateMachine->can($comment, 'publish')
+                    ? 'publish'
+                    : 'publish_ham'
+            );
         } else {
-            $comment->setState('published');
+            $this->logger->debug('Dropping comment message', [
+                'comment' => $comment->getId(),
+                'state' => $comment->getState(),
+            ]);
         }
-
-        $this->entityManager->flush();
     }
 }
