edcfc23fb133346e95c17074afeec793908e656a
UshurbakiyevDavlat <dushurbakiev@gmail.com>
2024-04-16 22:35:25 +0500

    added webpack and bootstrap for re-styling our templates

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
