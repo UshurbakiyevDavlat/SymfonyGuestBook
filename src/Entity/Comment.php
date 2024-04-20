f1873a74bb570e8ab0bd0ad547be753c476d282a
UshurbakiyevDavlat <dushurbakiev@gmail.com>
2024-04-20 12:17:19 +0500

    exposing api, added api resource for conference and comment entity, added filter published comment query extension to let it expose only those comments which were published

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -2,34 +2,51 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\CommentRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 #[ORM\HasLifecycleCallbacks]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => 'comment:item']),
+        new GetCollection(normalizationContext: ['groups' => 'comment:list'])
+    ],
+    order: ['created_at' => 'DESC'],
+    paginationEnabled: false,
+)]
 class Comment
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['comment:item','comment:list'])]
     private ?int $id = null;
 
     #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank]
+    #[Groups(['comment:item','comment:list'])]
     private string $author;
 
     #[ORM\Column(type: 'text')]
     #[Assert\NotBlank]
+    #[Groups(['comment:item','comment:list'])]
     private string $text;
 
     #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank]
     #[Assert\Email]
+    #[Groups(['comment:item','comment:list'])]
     private string $email;
 
     #[ORM\Column(type: 'datetime_immutable')]
+    #[Groups(['comment:item','comment:list'])]
     private DateTimeImmutable $created_at;
 
     #[ORM\ManyToOne(inversedBy: 'comments')]
@@ -37,9 +54,11 @@ class Comment
     private ?Conference $conference = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['comment:item','comment:list'])]
     private ?string $photoFilename = null;
 
     #[ORM\Column(length: 255, options: ['default' => 'submitted'])]
+    #[Groups(['comment:item','comment:list'])]
     private ?string $state = 'submitted';
 
     public function getId(): ?int
--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,34 +2,51 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[ORM\Entity(repositoryClass: ConferenceRepository::class)]
 #[UniqueEntity('slug')]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => 'conference:item']),
+        new GetCollection(normalizationContext: ['groups' => 'conference:list'])
+    ],
+    order: ['year' => 'DESC', 'city' => 'ASC'],
+    paginationEnabled: false,
+)]
 class Conference
 {
     #[ORM\Column(type: 'integer')]
     #[ORM\Id, ORM\GeneratedValue()]
+    #[Groups(['conference:list', 'conference:item'])]
     private int $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['conference:list', 'conference:item'])]
     private string $city;
 
     #[ORM\Column(type: 'integer')]
+    #[Groups(['conference:list', 'conference:item'])]
     private int $year;
 
     #[ORM\Column(type: 'boolean')]
+    #[Groups(['conference:list', 'conference:item'])]
     private bool $isInternational;
 
     #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'conference', orphanRemoval: true)]
     private Collection $comments;
 
     #[ORM\Column(length: 255, unique: true)]
+    #[Groups(['conference:list', 'conference:item'])]
     private ?string $slug = null;
 
     public function __construct()
